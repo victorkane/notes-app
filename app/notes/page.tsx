4c5d99d8ee103e79fd6774788d10f4786ec83941
victorkane <victorkane@gmail.com>
2023-10-07 19:18:21 -0300

    style(notes pages): show notes in style

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import styles from "./Notes.module.css"
+import CreateNote from "./CreateNote"
 
 async function getNotes() {
   const res = await fetch(
@@ -16,7 +17,7 @@ export default async function NotesPage() {
   return (
     <div>
       <h1>Notes</h1>
-      <div>
+      <div className={styles.grid}>
         {notes?.map((note) => {
           return <Note key={note.id} note={note} />
         })}
